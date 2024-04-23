05f59bdf24e3b914ef0b44616ab1cf056262ad80
Augusto Acual <augustoacual@gmail.com>
2024-04-23 09:39:34 -0600

    Directiva ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, DirectivaComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
