515adaf6e3e6d0c668c4c7ad06a4efde3e1445e0
Augusto Acual <augustoacual@gmail.com>
2024-04-26 12:30:40 -0600

    Crear el componente clientes y desplegar los clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { DirectivaComponent } from './directiva/directiva.component'
+import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, DirectivaComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, DirectivaComponent, ClientesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
