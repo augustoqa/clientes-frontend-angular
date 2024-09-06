c11dc60f769f147653bf6ed78837d248c17bbdbf
Augusto Quisquinay <augustoacual@gmail.com>
2024-09-06 13:15:24 -0600

    Implementar Servicio con HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './footer/footer.component'
 import { DirectivaComponent } from './directiva/directiva.component'
 import { ClientesComponent } from './clientes/clientes.component'
 import { RouterModule, Routes } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http'
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full' },
@@ -22,7 +23,7 @@ const routes: Routes = [
     DirectivaComponent,
     ClientesComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
